b05312785c3c0ad53f4030990b93ad92d314e510
Lanik123 <daniilt971@gmail.com>
2021-08-20 17:55:01 +0300

    clk: qcom: cpu-sdm: Checkout new version from 4.19
    Newer version of driver with minimal adaptation for 4.14.

--- include/dt-bindings/clock/qcom,cpu-sdm.h
+++ include/dt-bindings/clock/qcom,cpu-sdm.h
@@ -1,21 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
 /*
- * Copyright (c) 2020, The Linux Foundation. All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 and
- * only version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
  */
 
 #ifndef _DT_BINDINGS_CLK_QCOM_CPU_SDM_H
 #define _DT_BINDINGS_CLK_QCOM_CPU_SDM_H
 
-#define APCS_CPU_PLL		0
+#define APCS_CPU_PLL1		0
 #define APCS_MUX_C1_CLK		1
 #define APCS_MUX_CCI_CLK	2
+#define APCS_CPU_PLL0		3
+#define APCS_MUX_C0_CLK		4
 
 #endif
