3e33b6c4d9e30895a30da829947d91567fcc3255
MrArtemSid <artem.sid.key@gmail.com>
2021-08-24 00:03:21 +0400

    Revert "fbdev: msm: Handle rotator init failure correctly"
    This reverts commit 9e839098f015e398e248cd2a6f46ee535f06cf72.

--- drivers/video/fbdev/msm/mdss_rotator.c
+++ drivers/video/fbdev/msm/mdss_rotator.c
@@ -2216,16 +2216,14 @@ static int mdss_rotator_handle_request(struct mdss_rot_mgr *mgr,
 	ret = copy_from_user(items, user_req.list, size);
 	if (ret) {
 		pr_err("fail to copy rotation items\n");
-		devm_kfree(&mgr->pdev->dev, items);
-		return ret;
+		goto handle_request_err;
 	}
 
 	req = mdss_rotator_req_init(mgr, items, user_req.count, user_req.flags);
 	if (IS_ERR_OR_NULL(req)) {
 		pr_err("fail to allocate rotation request\n");
 		ret = PTR_ERR(req);
-		devm_kfree(&mgr->pdev->dev, items);
-		return ret;
+		goto handle_request_err;
 	}
 
 	mutex_lock(&mgr->lock);
@@ -2257,6 +2255,7 @@ static int mdss_rotator_handle_request(struct mdss_rot_mgr *mgr,
 
 handle_request_err1:
 	mutex_unlock(&mgr->lock);
+handle_request_err:
 	devm_kfree(&mgr->pdev->dev, items);
 	devm_kfree(&mgr->pdev->dev, req);
 	return ret;
@@ -2375,8 +2374,7 @@ static int mdss_rotator_handle_request32(struct mdss_rot_mgr *mgr,
 	ret = copy_from_user(items, compat_ptr(user_req32.list), size);
 	if (ret) {
 		pr_err("fail to copy rotation items\n");
-		devm_kfree(&mgr->pdev->dev, items);
-		return ret;
+		goto handle_request32_err;
 	}
 
 	req = mdss_rotator_req_init(mgr, items, user_req32.count,
@@ -2384,8 +2382,7 @@ static int mdss_rotator_handle_request32(struct mdss_rot_mgr *mgr,
 	if (IS_ERR_OR_NULL(req)) {
 		pr_err("fail to allocate rotation request\n");
 		ret = PTR_ERR(req);
-		devm_kfree(&mgr->pdev->dev, items);
-		return ret;
+		goto handle_request32_err;
 	}
 
 	mutex_lock(&mgr->lock);
@@ -2417,6 +2414,7 @@ static int mdss_rotator_handle_request32(struct mdss_rot_mgr *mgr,
 
 handle_request32_err1:
 	mutex_unlock(&mgr->lock);
+handle_request32_err:
 	devm_kfree(&mgr->pdev->dev, items);
 	devm_kfree(&mgr->pdev->dev, req);
 	return ret;
